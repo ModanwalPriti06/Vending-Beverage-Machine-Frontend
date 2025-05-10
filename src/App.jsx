3922d5ab049a00a12165ddf544d5abbb0b1a5a6f
Priti Modanwal <Pritimodanwal@naraci.com>
2025-05-10 23:08:14 +0530

    werfew

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,132 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import './App.css'
-
-const initialInventory = {
-  water: 10,
-  coffee: 10,
-  milk: 10,
-  sugar: 10,
-};
-
-const recipes = {
-  "Black Coffee": { water: 3, coffee: 1, sugar: 1 },
-  "Black Coffee (No Sugar)": { water: 3, coffee: 1 },
-  "Coffee with Milk": { water: 1, coffee: 1, milk: 2, sugar: 1 },
-  "Coffee with Milk (No Sugar)": { water: 1, coffee: 1, milk: 2 },
-};
 
 
 function App() {
-
-  const [inventory, setInventory] = useState(initialInventory);
+  const [inventory, setInventory] = useState([]);
   const [message, setMessage] = useState('');
+  const [bevIngredQty, setBevIngredQty] = useState({})
+  const [beverage, setBeverage] = useState([])
 
-  const canDispense = (recipe) =>
-    Object.entries(recipe).every(([ing, amt]) => inventory[ing] >= amt);
 
-  const dispense = (name) => {
-    const recipe = recipes[name];
-    if (!canDispense(recipe)) {
-      setMessage(`❌ Not enough ingredients for ${name}`);
-      return;
+  useEffect(() => {
+    getBeverage();
+    getInventory();
+  }, []);
+
+
+  const getInventory = async () => {
+    await axios.get("http://localhost:5000/api/inventory")
+      .then((res) => setInventory(res?.data))
+      .catch((err) => console.error("Inventory Error:", err));
+  }
+
+  const getBeverage = async () => {
+    await axios.get("http://localhost:5000/api/beverage")
+      .then((res) => setBeverage(res?.data))
+      .catch((err) => console.error("Beverages Error:", err));
+  }
+
+  // dispense beverage 
+  const dispense = async (id, name) => {
+    try {
+      const { data } = await axios.post(
+        'http://localhost:5000/api/dispense_beverage',
+        { beverageId: id }
+      );
+        setBevIngredQty(data);
+        await getInventory();
+        setMessage(`✅ Dispensed: ${name}`);
+    } catch (err) {
+      console.error("Dispense Error:", err);
+      setMessage(`❌ Failed to dispense ${name}`);
     }
-
-    const updatedInventory = { ...inventory };
-    for (let ing in recipe) {
-      updatedInventory[ing] -= recipe[ing];
+  };
+  
+  //add ingredients
+  const topUp = (id, value) => {
+    let obj = {
+      id: id,
+      value: value
     }
-    setInventory(updatedInventory);
-    setMessage(`✅ Dispensed: ${name}`);
-  };
+    axios.post('http://localhost:5000/api/add_ingredient', obj)
+      .then((res) => {
+        console.log('Added successfully!');
+        getInventory();
+      })
+      .catch((err) => console.error(err))
+  }
 
-  const topUp = (ing, amt) => {
-    setInventory(prev => ({
-      ...prev,
-      [ing]: prev[ing] + amt,
-    }));
-  };
 
   return (
     <div className="p-6 max-w-3xl mx-auto font-sans">
-       <div className="bg-[#8B4513] text-white rounded-xl shadow-lg p-6">
+      <div className="bg-[#8B4513] text-white rounded-xl shadow-lg p-6">
+        <h1 className="text-3xl font-bold mb-6 text-center">☕ Beverage Vending Machine</h1>
 
-      <h1 className="text-3xl font-bold mb-6 text-center">☕ Beverage Vending Machine</h1>
-
-      {/* Beverage Buttons */}
-      <div className="grid grid-cols-2 gap-4 mb-6">
-        {Object.keys(recipes).map(name => (
-          <button
-            key={name}
-            onClick={() => dispense(name)}
-            disabled={!canDispense(recipes[name])}
-            className={`p-4 rounded-lg shadow-md transition-colors ${canDispense(recipes[name])
-                ? 'bg-green-500 hover:bg-green-600 text-white'
-                : 'bg-gray-300 text-gray-500 cursor-not-allowed'
-              }`}
-          >
-            {name}
-          </button>
-        ))}
-      </div>
-
-      {/* Message */}
-      {message && (
-        <div className="mb-4 text-center text-lg font-medium">
-          {message}
-        </div>
-      )}
-
-      {/* Inventory Display */}
-      <div className="bg-amber-100 p-4 rounded-md mb-6">
-        <h2 className="text-xl font-bold mb-2 text-yellow-900"> Inventory.. </h2>
-        <ul className="space-y-1">
-          {Object.entries(inventory).map(([ing, amt]) => (
-            <li key={ing} className="flex justify-between text-yellow-900">
-              <span>{ing}</span>
-              <span>{amt} units</span>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {/* Top-up Section */}
-      <div className="bg-blue-50 p-4 rounded-md">
-        <h2 className="text-xl font-semibold mb-2 text-blue-600">🔼 Top Up Ingredients</h2>
-        <div className="grid grid-cols-2 gap-4">
-          {Object.keys(inventory).map(ing => (
+        {/* Beverage Buttons */}
+        <div className="grid grid-cols-2 gap-4 mb-6">
+          {beverage?.map((item, idx) => (
             <button
-              key={ing}
-              onClick={() => topUp(ing, 5)}
-              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+              key={idx}
+              onClick={() => dispense(item?._id, item?.name)}
+              // disabled={!canDispense(recipes[name])}
+              className={'p-4 rounded-lg shadow-md transition-colors bg-green-500 hover:bg-green-600 text-white'
+              }
             >
-              +5 units {ing}
+              {item.name}
             </button>
           ))}
         </div>
-      </div>
+
+        {/* Message */}
+        {message && (
+          <div className="mb-4 text-center text-lg font-medium">
+            {message}
+          </div>
+        )}
+
+        {/* beverage ingredient */}
+        {bevIngredQty && (
+          <div className="mb-4 text-center text-lg font-medium">
+            {Object.entries(bevIngredQty).map(([key, vale]) => (
+              <div key={key} className="inline-block mx-2">
+                <span className="font-semibold">{key}:</span>{" "}
+                <span>{vale}</span>
+              </div>
+            ))}
+          </div>
+        )}
+
+
+        {/* Inventory Display */}
+        <div className="bg-amber-100 p-4 rounded-md mb-6">
+          <h2 className="text-xl font-bold mb-2 text-yellow-900"> Inventory </h2>
+          <ul className="space-y-1">
+            {inventory?.map((item, key) => (
+              <li key={item} className="flex justify-between text-yellow-900">
+                <span>{item.name}</span>
+                <span>{item.value} units</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Top-up Section */}
+        <div className="bg-blue-50 p-4 rounded-md">
+          <h2 className="text-xl font-semibold mb-2 text-blue-600">🔼 Top Up Ingredients</h2>
+          <div className="grid grid-cols-2 gap-4">
+            {inventory?.map((item, idx) => (
+              <button
+                key={idx}
+                onClick={() => topUp(item._id, 5)}
+                className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+              >
+                +5 units {item.name}
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
